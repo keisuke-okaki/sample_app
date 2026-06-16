f6513c6291c570fbd0055475dd6318305a0dd564
keisuke-okaki <keisuke.okaki@giftee.co>
2026-06-16 17:17:45 +0900

    add layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get  "static_pages/home"
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get "application/hello"
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
 end
