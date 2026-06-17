4c34df0e2cb4771495c7b2529a6af0545654b0c0
keisuke-okaki <keisuke.okaki@giftee.co>
2026-06-17 13:40:36 +0900

    add practice 6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -36,7 +36,7 @@ class UserTest < ActiveSupport::TestCase
   end
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                           foo@bar_baz.com foo@bar+baz.com]
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com foo@.co.jp]
     invalid_addresses.each do |invalid_address|
       @user.email = invalid_address
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
@@ -56,5 +56,12 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "email should be downcase" do
+    email_downcase = @user.email.downcase
+    @user.email = @user.email.upcase
+    @user.save
+    assert_equal email_downcase, @user.reload.email 
   end
 end
