337191794ea40ac3dafed85539570a93bd3924c5
keisuke-okaki <keisuke.okaki@giftee.co>
2026-07-17 10:37:24 +0900

    miss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://https://sample-app-tbff.onrender.com'
+  host = 'https://sample-app-tbff.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 2525,
